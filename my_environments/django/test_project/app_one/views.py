0b0f60853b3618c70e99fb90fa51dfb57632696a
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-07-28 12:37:09 -0700

    Updating completed django first_app

--- my_environments/django/test_project/app_one/views.py
+++ my_environments/django/test_project/app_one/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
+import random
 
 # Create your views here.
 def index(request):
@@ -11,16 +12,16 @@ def new(request):
 def create(request):
     print("you created a blog post")
     print(request.POST)
-    print(request.post['p_name'])
-    print(request.post['b_post'])
+    print(request.POST['p_name'])
+    print(request.POST['b_post'])
     request.session['blog_post'] = request.POST['b_post']
     request.session['first_name'] = request.POST['p_name']
     return redirect('/')
 
 def show(request, number):
     context = {
-        'blog_id':number
-        'time': strftime("%Y-%m-%d %H:%M %p", gtime()),
+        'blog_id':number,
+        'time': strftime("%Y-%m-%d %H:%M %p", gmtime()),
         'randy': int(random.random() * 100)
     }
     return render(request, 'one_blog.html', context)
@@ -30,6 +31,3 @@ def edit(request, number):
 
 def destroy(request, number):
     return HttpResponse(f"This is a placeholder to delete blog: {number}")
-
-def index(request):
-    return render(request, "index.html")
