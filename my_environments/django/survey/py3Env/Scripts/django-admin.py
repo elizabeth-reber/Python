6d53e27da398ccad7e013725817260acbbe59441
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-07-28 19:54:09 -0700

    Survey

---
+++ my_environments/django/survey/py3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\eliza\OneDrive\Desktop\python_stack\my_environments\django\survey\py3Env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
