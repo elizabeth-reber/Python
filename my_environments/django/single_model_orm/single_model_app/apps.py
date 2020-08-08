0eea947125ff28b66c705d1a754133078ac347c7
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-08-08 13:29:02 -0700

    add my project

---
+++ my_environments/django/single_model_orm/single_model_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SingleModelAppConfig(AppConfig):
+    name = 'single_model_app'
---
+++ my_environments/django/single_model_orm/single_model_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 225)
+    last_name = models.CharField(max_length = 225)
+    email_address = models.CharField(max_length = 225)
+    age = models.IntegerField(max_length = 225)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updates_at = models.DateTimeField(auto_now=True)
