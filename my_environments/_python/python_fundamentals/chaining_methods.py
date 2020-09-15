fedafec5f59494ccd9ac41c6a114e51527033d7b
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-09-14 18:33:37 -0700

    chaining methods

---
+++ my_environments/_python/python_fundamentals/chaining_methods.py
@@ -0,0 +1,42 @@
+
+#creating a file with User Class including the __inti__ and make_deposit methods
+class User:
+    def __init__(self):
+        self.name = "Michael"
+        self.email = "michael@codingdojo.com"
+        self.account_balance = 0
+
+#make_deposit method
+    def make_deposit(self, amount):	
+    	self.account_balance += amount
+
+# make_withdrawal method
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+
+#add a display_user_balance method to User class
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}")
+
+#Create three instances of user class
+elizabeth.name = "Elizabeth", "elizabeth.szathmary@gmail.com"
+rowley.name = "Rowley", "rowleyismycat@gmail.com"
+hamish.name = "Hamish", "hamishisacharacterfromtv@gmail.com"
+
+#Have the first user make 3 deposits, 1 withdrawal and display balance
+elizabeth.make_deposit(100,000).make_deposit(100,000).make_deposit(100,000).make_withdrawal(50,000).display_user_balance()
+print(f"User: {self.name}, Balance: ${self.account_balance}", "Now you are rich.  Go party like a rock star.  Actually don't.  They all end up in rehab.  Instead invest wisely, so that you can retire young and help disadvantaged kids go to college.")
+
+# Have the second user make 2 deposits and 2 withdrawals and then display their balance
+rowley.make_deposit(100.00).make_deposit(100.00).make_withdrawl(100.00).make_withdrawal(50.00).display_user_balance()
+print(f"User: {self.name}, Balance: ${self.account_balance}", "Good kitty")
+
+# Have the third user make 1 deposits and 3 withdrawals and then display their balance   
+Hamish.make_deposit(10.00).make_deposit(5.00).make_withdrawl(1.00).make_withdrawal(1.00).display_user_balance()
+print(f"User: {self.name}, Balance: ${self.account_balance}", "You're poor.  You should probably apply for some scholarships and grants, go back to school, and make something of yourself because this ballance is just sad.")
+
+# BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
+elizabeth.transfer_money(hamish,250)
+hamish.display_user_balance()
+elizabeth.display_user_balance()
+
