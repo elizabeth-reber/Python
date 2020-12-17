937f62de9187c782d8fc66f1ee0a70971bf67082
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-12-17 15:00:37 -0800

    login and registration

---
+++ my_environments/django/log_reg/log_reg_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors["first_name"] = "First name should be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "Last name should be at least 2 characters"
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
