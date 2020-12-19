753710bf948eaacd2fb1e6a8fe7a75854e83b9ac
elizabeth-reber <elizabeth.szathmary@gmail.com>
2020-12-19 15:01:44 -0800

    the wall

--- my_environments/django/tv_shows_proj/tv_shows_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TvShowsAppConfig(AppConfig):
-    name = 'tv_shows_app'
--- my_environments/django/tv_shows_proj/tv_shows_app/pip_practice.py
+++
@@ -1,9 +0,0 @@
-print ("Hello World")
-name = "Elizabeth"
-print("hello", name)
-print("hello " + name)
-name = 42
-print("hello", name)
-fav_food1 = "sushi"
-fav_food2 = "pizza"
-print("I love to eat {} and {}.".format(fav_food1, fav_food2))
